29b2356196ce97a484e59c91e101dcff22701f1a
Meghana2013 <120323535+Meghana2013@users.noreply.github.com>
2026-08-24 23:20:21 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -4,7 +4,7 @@ import os
 def generate_sql(question):
     llm = ChatGroq(
         groq_api_key=os.getenv("GROQ_API_KEY"),
-        model_name="llama-3.1-8b-instant"
+        model_name="openai/gpt-oss-20b"
     )
 
     prompt = f"""
